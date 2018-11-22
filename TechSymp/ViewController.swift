da5881e5d70853d01527f8934b56615005d5903e
Karthik B S <kokarbs@gmail.com>
2018-11-22 11:28:02 +0530

    Initial Commit

---
+++ TechSymp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechSymp
+//
+//  Created by Simpro on 22/11/18.
+//  Copyright © 2018 k4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
